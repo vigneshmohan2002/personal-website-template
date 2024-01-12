795fc9e2b9971ed6140940e1835b87033ded9b7d
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2024-01-12 18:47:02 +0000

    Added resume

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -66,7 +66,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: './assets/resume.pdf',
+      href: '/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
